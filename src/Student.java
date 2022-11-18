4c619a51095f28fdb980168790848501c14d8045
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-17 21:31:18 -0600

    added attribute, constructor and toString in Student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,27 @@
 public class Student {
+    private String name;
+    private String classBlock;
+    private int dailyBehaviorScore;
+    private int date;
+    private int weeklyAverage;
+
+    public Student(String name, String classBlock, int dailyBehaviorScore, int date) {
+        this.name = name;
+        this.classBlock = classBlock;
+        this.dailyBehaviorScore = dailyBehaviorScore;
+        this.date = date;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classBlock='" + classBlock + '\'' +
+                ", dailyBehaviorScore=" + dailyBehaviorScore +
+                ", date=" + date +
+                '}';
+    }
+
+
 }
