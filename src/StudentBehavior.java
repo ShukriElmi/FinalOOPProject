e9156fda683b47b097d4fdb5d2deeefe2fb60e52
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-21 10:54:09 -0600

    created a super and subclass

--- src/StudentBehavior.java
+++ src/StudentBehavior.java
@@ -1,2 +1,7 @@
-public class StudentBehavior {
+public class StudentBehavior extends Student {
+
+    public StudentBehavior(String name, String classBlock, int dailyBehaviorScore, int date) {
+        super(name, classBlock, dailyBehaviorScore, date);
+
+    }
 }
