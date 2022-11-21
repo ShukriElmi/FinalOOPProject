9436d93465ad491cc648f966ddc059147ed05cae
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-21 11:18:43 -0600

    added attribute and a hashmap to the StudentBehavior class

--- src/StudentBehavior.java
+++ src/StudentBehavior.java
@@ -1,7 +1,18 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
 public class StudentBehavior extends Student {
+    static Scanner scan = new Scanner(System.in);
+//    static StudentBehavior shukriClass = new StudentBehavior(String name, String classBlock, int dailyBehaviorScore, int date);
+    static String option = "";
+
+    HashMap<Integer, Student> studentGrades;
 
     public StudentBehavior(String name, String classBlock, int dailyBehaviorScore, int date) {
         super(name, classBlock, dailyBehaviorScore, date);
+        this.studentGrades = new HashMap<Integer, Student>();
 
     }
+
+
 }
